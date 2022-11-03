dbfa1bed030498d5b8f325ee10fe3ef9b9992c7a
TandiweP <87207713+TandiweP@users.noreply.github.com>
2022-11-03 17:53:31 -0600

    footer solid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import scroll from "../images/scroll.png"
 export default function Footer() {
     return (
         <div className="Footer" id ="pageFooter">
-            <img className="scroll" src={scroll} alt="scroll" />
+            {/* <img className="scroll" src={scroll} alt="scroll" /> */}
             
         </div>
     )
